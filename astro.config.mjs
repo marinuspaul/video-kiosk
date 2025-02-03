b1f6c7ac22aab7d00e644a05796190b8ad8fc353
marinuspaul <60815838+marinuspaul@users.noreply.github.com>
2025-02-03 21:10:47 +0100

    load data from json setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
-
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [relativeLinks()]
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+  integrations: [relativeLinks()],
 });
