2b4c9dcf5f8d19aa741ad3877a567d1f5d33d113
marinuspaul <60815838+marinuspaul@users.noreply.github.com>
2025-02-06 14:34:59 +0100

    load store everything in json and local storage

---
+++ public/data.js
@@ -0,0 +1,20 @@
+let data = [
+  {
+    "author": "Person A",
+    "title": "toller Film",
+    "section": "toller Kurs",
+    "file": "toller-Film"
+  },
+  {
+    "author": "Person B",
+    "title": "großartiger Film2",
+    "section": "anderer toller Kurs",
+    "file": "großartiger-Film2"
+  },
+  {
+    "author": "Person C, Person D",
+    "title": "Meisterwerk",
+    "section": "toller Kurs",
+    "file": "Meisterwerk"
+  }
+]
